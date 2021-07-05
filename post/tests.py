14974e7ecc0f9ffd1cb7f1f145588c18a043514d
sjwhole <bluemoon2718@naver.com>
2021-07-05 13:10:24 +0900

    test: Add CRUD post test

--- post/tests.py
+++ post/tests.py
@@ -1,16 +1,43 @@
-import json
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-from django.test import Client, TestCase
-
-client = Client()
+from .models import Post
 
 
-class PostTest(TestCase):
+class PostTests(APITestCase):
     def test_create_post(self):
-        data = {
-            "grade": "5",
-            "body": "내용입니다",
-        }
+        url = reverse("post-list")
+        data = {"grade": "5", "body": "Good"}
+        response = self.client.post(url, data, format="json")
 
-        response = client.post("/posts/", json.dumps(data), content_type="application/json")
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 1)
+        self.assertEqual(Post.objects.get().body, "Good")
+
+    def test_get_post(self):
+        Post.objects.create(id=1, grade=5, body="Good")
+
+        response = self.client.get("/posts/1/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("id"), 1)
+        self.assertEqual(response.data.get("grade"), 5)
+        self.assertEqual(response.data.get("body"), "Good")
+
+    def test_patch_post(self):
+        Post.objects.create(id=1, grade=5, body="Good")
+
+        data = {"grade": 4, "body": "Nice"}
+        response = self.client.patch("/posts/1/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("id"), 1)
+        self.assertEqual(response.data.get("grade"), 4)
+        self.assertEqual(response.data.get("body"), "Nice")
+
+    def test_delete_post(self):
+        Post.objects.create(id=1, grade=5, body="Good")
+
+        response = self.client.delete("/posts/1/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
