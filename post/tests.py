55393d7ba6aae582e457326021929e9c6d48d648
sjwhole <bluemoon2718@naver.com>
2021-07-05 10:51:32 +0900

    test: Add post create test

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import Client, TestCase
+
+client = Client()
+
+
+class PostTest(TestCase):
+    def test_create_post(self):
+        data = {
+            "grade": "5",
+            "body": "내용입니다",
+        }
+
+        response = client.post("/posts/", json.dumps(data), content_type="application/json")
+        self.assertEqual(response.status_code, 201)
