14b210cc400a41d2d9f7779eaa3bcf4da5ff5b9b
sjwhole <bluemoon2718@naver.com>
2021-07-05 13:15:57 +0900

    feat: Add post admin model

--- post/admin.py
+++ post/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "grade", "body", "created_at")
+
+
+admin.site.register(Post, PostAdmin)
--- post/models.py
+++ post/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     body = models.TextField()
     grade = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
