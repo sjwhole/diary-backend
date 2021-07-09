c111c323fdde50493f53bdc2225438acb14f9cd4
sjwhole <bluemoon2718@naver.com>
2021-07-09 19:30:05 +0900

    feat: Add my post url

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import PostViewSet
+from .views import PostViewSet, MyPostView
 
 router = DefaultRouter()
 router.register("", PostViewSet)
 
 urlpatterns = [
+    path("my/", MyPostView.as_view()),
     path("", include(router.urls)),
 ]
