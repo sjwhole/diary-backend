9e54368b2464297919ba88978dd0cd85aee3183b
sjwhole <bluemoon2718@naver.com>
2021-07-09 20:49:25 +0900

    feat: Add post urls

--- post/urls.py
+++ post/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import PostViewSet, MyPostView
+from .views import PostCreateViewSet, PostReadUpdateDeleteView, PostSharedViewSet, MyPostView
 
 router = DefaultRouter()
-router.register("", PostViewSet)
+router.register("", PostSharedViewSet)
 
 urlpatterns = [
     path("my/", MyPostView.as_view()),
-    path("", include(router.urls)),
+    path("<int:pk>/", PostReadUpdateDeleteView.as_view()),
+    path("", PostCreateViewSet.as_view()),
+    path("share/", include(router.urls)),
 ]
