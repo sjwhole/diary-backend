b4f8ed61c1618dd4f402512ba78117aa8ae50a33
sjwhole <bluemoon2718@naver.com>
2021-07-09 19:29:34 +0900

    feat: Add user to post model

--- post/models.py
+++ post/models.py
@@ -1,8 +1,11 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+from user.models import User
+
 
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     grade = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     created_at = models.DateField(auto_now_add=True)
